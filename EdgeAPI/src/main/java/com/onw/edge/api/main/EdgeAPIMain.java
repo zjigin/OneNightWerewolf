b6a44b123e09478cdbe0a406d5aeefa1f1252325
zji <zjigin@gmail.com>
2018-09-20 12:20:04 -0400

    Disable Eureka for now.

--- EdgeAPI/src/main/java/com/onw/edge/api/main/EdgeAPIMain.java
+++ EdgeAPI/src/main/java/com/onw/edge/api/main/EdgeAPIMain.java
@@ -2,11 +2,9 @@ package com.onw.edge.api.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableEurekaClient
 @ComponentScan("com.onw.edge.api")
 public class EdgeAPIMain {
 
